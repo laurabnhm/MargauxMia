b3ae025713d9f13a99d889cb54b0cf3dd7657580
laurabnhm <laura.bnhm@gmail.com>
2023-04-18 10:10:53 -0400

    [HOME] son logo ok

--- js/home.js
+++ js/home.js
@@ -1,17 +1,27 @@
 let sound_site = document.querySelector('.sound-file-site');
 let sound_logo = document.querySelector('.sound-file-logo');
 
-document.querySelector('.img-sound').addEventListener('click', playSoundSite);
-sound_site.play();
+
+
+/*********** SON SITE ***********/
+
+document.querySelector('.img-sound-site').addEventListener('click', playSoundSite);
 
 function playSoundSite() {
     if (sound_site.paused) {
         sound_site.play();
-        document.querySelector('.img-sound').src = 'img/volume-high.svg';
-        console.log('son play');
+        document.querySelector('.img-sound-site').src = 'img/volume-high.svg';
     } else {
         sound_site.pause();
-        document.querySelector('.img-sound').src = 'img/volume-mute.svg';
-        console.log('son off');
+        document.querySelector('.img-sound-site').src = 'img/volume-mute.svg';
     }
+}
+
+
+/*********** SON LOGO ***********/
+
+document.querySelector('.img-symbole').addEventListener('click', playSoundLogo);
+
+function playSoundLogo() {
+    sound_logo.play();
 }
